a298d8d48319bba12c8c81988f04ce18fcf0e846
Bugo <bugo@dragomano.ru>
2022-06-05 19:02:36 +0500

    Update to v0.5

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Happy Birthday
-![SMF 2.1](https://img.shields.io/badge/SMF-2.1-ed6033.svg?style=flat)
+[![SMF 2.1](https://img.shields.io/badge/SMF-2.1-ed6033.svg?style=flat)](https://github.com/SimpleMachines/SMF2.1)
 ![License](https://img.shields.io/github/license/dragomano/happy-birthday)
+![Hooks only: Yes](https://img.shields.io/badge/Hooks%20only-YES-blue)
 
-* **Author:** Bugo [dragomano.ru](https://dragomano.ru/mods/happy-birthday)
-* **License:** [MIT](https://github.com/dragomano/Happy-Birthday/blob/master/LICENSE)
-* **Compatible with:** SMF 2.1.*
-* **Hooks only:** Yes
+* **Tested on:** PHP 7.4.29, MariaDB 10.6.7
 * **Languages:** Danish, English, German, Italian, Russian, Turkish, Ukrainian
 
 ## Description
-Display a cake on the right of the messages of users who have a birthday today.
+Displays a cake on the right side of the messages of users who have a birthday today.
 
 ## Описание
 Демонстрация сладких угощений в правой части сообщений пользователей, у которых наступает день рождения.
 
+![](https://user-images.githubusercontent.com/229402/172054312-6fab5bac-30a6-4eca-ad85-449f7ec90468.png)
--- Sources/Class-HappyBirthday.php
+++ Sources/Class-HappyBirthday.php
@@ -4,12 +4,12 @@
  * Class-HappyBirthday.php
  *
  * @package HappyBirthday
- * @link https://dragomano.ru/mods/happy-birthday
+ * @link https://github.com/dragomano/Happy-Birthday
  * @author Bugo <bugo@dragomano.ru>
- * @copyright 2010-2020 Bugo
+ * @copyright 2010-2022 Bugo
  * @license https://opensource.org/licenses/MIT The MIT License
  *
- * @version 0.4
+ * @version 0.5
  */
 
 if (!defined('SMF'))
@@ -17,11 +17,14 @@ if (!defined('SMF'))
 
 class HappyBirthday
 {
-	public static function prepareDisplayContext(&$output)
+	public function prepareDisplayContext(&$output)
 	{
 		global $context, $scripturl, $settings, $txt;
 
-		if (!$context['user']['is_guest'] && isset($output['member']['birth_date']) && (substr($output['member']['birth_date'], 5, 5) == date('m-d')) && !strpos($output['body'], 'code')) {
+		if ($context['user']['is_guest'] || empty($output['member']['birth_date']))
+			return;
+
+		if (substr($output['member']['birth_date'], 5, 5) == date('m-d') && !strpos($output['body'], 'code')) {
 			loadLanguage('HappyBirthday/');
 
 			$output['body'] .= '<div class="floatright"><a href="' . $scripturl. '?action=profile;u=' . $output['member']['id'] . '"><img src="' . $settings['default_images_url'] . '/sweet/' . rand(1,26) . '.png" width="64" height="64" alt="' . $txt['happy_birthday_text'] . '" title="' . $txt['happy_birthday_text'] . '"></a></div>';
